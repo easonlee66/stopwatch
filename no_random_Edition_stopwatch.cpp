82e2698659f152ffecb21060bdf7e7c479273d2c
easonlee66 <62692775+easonlee66@users.noreply.github.com>
2020-04-04 12:05:22 +0800

    扩充计次范围（由原来的10变为100）
    Expand the counting range (from 10 to 100)

--- no_random_Edition_stopwatch.cpp
+++ no_random_Edition_stopwatch.cpp
@@ -8,7 +8,7 @@ using namespace std;
 struct timex{
 	int fen,miao,hm;
 };//Structure representing time
-timex jc[11];//Record count information
+timex jc[100];//Record count information
 int now_number;//How many times is the record now
 int main(){
 	now_number=1;
@@ -18,7 +18,8 @@ int main(){
 			int how=getch()-'0';
 			if(how==1){
 				now_number++;
-				if(now_number>10){
+				if(now_number>100){
+					MessageBox(NULL,"too much record!!","Error",MB_OK);
 					exit(3322577);
 				}
 				jc[now_number]=jc[now_number-1];
