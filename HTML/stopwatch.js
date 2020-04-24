b4315d16c90b9fe4c542bf5d1565cface0edd187
easonlee <62692775+easonlee66@users.noreply.github.com>
2020-04-24 11:32:45 +0800

    Rewrite stopwatch.js

--- HTML/stopwatch.js
+++ HTML/stopwatch.js
@@ -1,7 +1,22 @@
-$(document).ready(function(){
-  $(iuput).keydown(function(){
+var jc=new Array();
+var nownumner=0;
+jc[1]=0;
+function start(){
+}
+function outputtime(){
+}
+$("#start").click(function(){
+  start();
+  $("input").keydown(function(event){
+    var key=event.witch;
+    key=key-48;
+    if(key==1){
+    }
+    else{
+    }
   });
-  $(input).keyup(function(){
+  $("input").keyup(function(){
+    outputtime();
   });
 });
 //As you can see, I'm mimicking the C++ version
