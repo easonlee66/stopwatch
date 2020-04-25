e76873d71f1678ea14739506f2de3d574586eea1
easonlee <62692775+easonlee66@users.noreply.github.com>
2020-04-25 18:50:27 +0800

    Update stopwatch.js

--- HTML/stopwatch.js
+++ HTML/stopwatch.js
@@ -2,21 +2,21 @@ $(document).ready(function() {
 	var jc = new Array();
 	var nownumner = 1;
 	jc[1] = 0;
-  var startt;
-  function record(){
-    nownumber = nownumber + 1;
-    let newnode = document.createElement("p");
-    let id = document.createAttribute("id");
-    id.value = ("id" + nownumner);
-    newnode.setAttribute(id);
-    let text = document.createTextNode("");
-    newnode.appendChild(text);
-    document.getElementById("times").appendChild(newnode);
-  }
+  	var startt;
+  	function record(){
+  	  nownumber = nownumber + 1;
+  	  let newnode = document.createElement("p");
+  	  let id = document.createAttribute("id");
+  	  id.value = ("id" + nownumner);
+  	  newnode.setAttribute(id);
+  	  let text = document.createTextNode("");
+  	  newnode.appendChild(text);
+ 	   document.getElementById("times").appendChild(newnode);
+ 	 }
 	function start() {
 		let dat = new Date();
-    startt = dat.getTime();
-    record();
+	    startt = dat.getTime();
+	    record();
 	}
 	function outputtime() {
 		for (let i = nownumber; i > 0; i = i - 1) {
@@ -26,18 +26,18 @@ $(document).ready(function() {
 	}
 	var pausetime = 0;
 	$("#start").click(function() {
-    start();
-    document.getElementById("information").innerHTML="Press 1 to recoud,press 2 to pause";
-    var pause=false;
-		$("#key").keydown(function(event) {
-      document.getElementById("key").value="";
-			var key = event.witch;
-			key = key - 48;
-			if (key == 1) {
+	    start();
+	    document.getElementById("information").innerHTML="Press 1 to recoud,press 2 to pause";
+	    var pause=false;
+            $("#key").keydown(function(event) {
+   	   document.getElementById("key").value="";
+ 	   var key = event.witch;
+	   key = key - 48;
+	   if (key == 1) {
 				if (pause) {
 					start();
 				} else {
-          record();
+			          record();
 				}
 			} else {
 				pause = !pause;
@@ -59,3 +59,4 @@ $(document).ready(function() {
 		});
 	});
 	//As you can see, I'm mimicking the C++ version
+});
